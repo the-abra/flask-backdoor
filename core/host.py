ef9478a8a56f8852d6b3ae58cc0e38e8636003ba
the-abra <aegean.sword1@gmail.com>
2024-05-27 20:57:21 +0300

    p

--- core/host.py
+++ core/host.py
@@ -9,4 +9,4 @@ def home():
 if __name__ == '__main__':
     # Run the Flask app on a specific port
     port = 467  # You can change this to any port you prefer
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='127.0.0.1', port=port, debug=True)
